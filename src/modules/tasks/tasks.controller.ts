7b82feb4aa1dc6a967736608e0a9c1741f550b75
ak1103dev <ak.apichan1103@gmail.com>
2022-03-26 12:07:19 +0700

    :construction: open api

--- src/modules/tasks/tasks.controller.ts
+++ src/modules/tasks/tasks.controller.ts
@@ -1,4 +1,9 @@
 import {
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import {
   Body,
   Controller,
   Get,
@@ -8,28 +13,36 @@ import {
   Post,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { CreateTaskDto, UpdateTaskDto } from './tasks.dto';
+import { CreateTaskDto, UpdateTaskDto, Task } from './tasks.dto';
+import { ExceptionResponse } from 'src/utils/error.dto';
 
+@ApiTags('tasks')
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: Task })
   create(@Body() data: CreateTaskDto) {
     return this.tasksService.create(data);
   }
 
   @Get()
+  @ApiCreatedResponse({ type: [Task] })
   find() {
     return this.tasksService.find();
   }
 
   @Get(':id')
+  @ApiCreatedResponse({ type: Task })
+  @ApiNotFoundResponse({ type: ExceptionResponse })
   findById(@Param('id', ParseIntPipe) taskId: number) {
     return this.tasksService.findById(taskId);
   }
 
   @Patch(':id')
+  @ApiCreatedResponse({ type: Task })
+  @ApiNotFoundResponse({ type: ExceptionResponse })
   async update(
     @Param('id', ParseIntPipe) taskId: number,
     @Body() data: UpdateTaskDto,
--- src/modules/tasks/tasks.dto.ts
+++ src/modules/tasks/tasks.dto.ts
@@ -1,25 +1,46 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsBoolean, IsOptional, IsString } from 'class-validator';
 
+export class Task {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  title: string;
+
+  @ApiPropertyOptional()
+  description?: string;
+
+  @ApiProperty()
+  isDone: boolean;
+}
+
 export class CreateTaskDto {
+  @ApiProperty()
   @IsString()
   title: string;
 
+  @ApiPropertyOptional()
   @IsString()
   @IsOptional()
   description?: string;
 
+  @ApiProperty()
   @IsBoolean()
   isDone: boolean;
 }
 
 export class UpdateTaskDto {
+  @ApiPropertyOptional()
   @IsString()
   title: string;
 
+  @ApiPropertyOptional()
   @IsString()
   @IsOptional()
   description?: string;
 
+  @ApiPropertyOptional()
   @IsBoolean()
   isDone: boolean;
 }
