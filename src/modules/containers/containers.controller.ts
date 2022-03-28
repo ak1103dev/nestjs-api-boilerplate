9ef938c8e45a50c929a3d8e9897b6789182f182f
ak1103dev <ak.apichan1103@gmail.com>
2022-03-28 23:09:36 +0700

    :construction: download file

--- src/modules/containers/containers.controller.ts
+++ src/modules/containers/containers.controller.ts
@@ -1,11 +1,17 @@
 import { ApiTags } from '@nestjs/swagger';
 import {
   Controller,
+  Get,
   Post,
   UploadedFile,
   UseInterceptors,
+  StreamableFile,
+  Param,
+  NotFoundException,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { createReadStream, existsSync } from 'fs';
+import { join } from 'path';
 
 @ApiTags('containers')
 @Controller('containers')
@@ -14,5 +20,16 @@ export class ContainersController {
   @UseInterceptors(FileInterceptor('file'))
   uploadFile(@UploadedFile() file: Express.Multer.File) {
     return file;
+  }
+
+  @Get('download/:filename')
+  downloadFile(@Param('filename') filename: string) {
+    const filePath = `storages/${filename}`;
+    const existFile = existsSync(filePath);
+    if (!existFile) {
+      throw new NotFoundException('file not found');
+    }
+    const file = createReadStream(join(process.cwd(), filePath));
+    return new StreamableFile(file);
   }
 }
